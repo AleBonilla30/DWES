1fea187ce2bb964bbf3b5975a216f4e9560280f1
AleBonilla30 <ale.bonilla309@gmail.com>
2024-10-22 14:18:46 +0200

    update ejercicio6

--- Ejemplos_Clase/Ejercicios/ejercicio6.php
+++ Ejemplos_Clase/Ejercicios/ejercicio6.php
@@ -1,11 +1,33 @@
 <?php
 /* Un programa que genere 2 tiradas de 3 dados(simulando 2 jugadores). 
 Muestre las dos tiradas y me diga cual tiene mayor puntuación(sumando las tiradas) */
-$contador = 0;
-$tirada = false;
-for ($i=0; $i <3 ; $i++) { 
-    $dado1 = rand(1,6);
+$totalTirada1 = 0;
+$totalTirada2 = 0;
+print "<h1>Tirada 1</h1>\n";
+for ($i=0; $i < 3 ; $i++) { 
+    $dado = rand(1,6);
+    print "<img src ='./img/$dado.jpg' width =100 height = 100/>\n";
+    $totalTirada1 += $dado;
 
+}
+print "<h3>Has obtenido  $totalTirada1 puntos en la tirada 1</h3></br>";
+
+
+print "<h1>Tirada 2</h1>\n";
+for ($i=0; $i < 3 ; $i++) { 
+    $dado = rand(1,6);
+    print "<img src ='./img/$dado.jpg' width =100 height = 100/>\n";
+    $totalTirada2 += $dado;
+
+}
+print "<h3>Has obtenido  $totalTirada2 puntos en la tirada 1</h3</br>";
+
+if ($totalTirada1 > $totalTirada2) {
+    print "<h4>La tirada uno es la mas alta tiene un total de $totalTirada1 puntos.</h4><br>"; 
+}elseif ($totalTirada2 > $totalTirada1) {
+    print "<h4>La tirada dos es la mas alta tiene un total de $totalTirada2 puntos.</h4><br>"; 
+}else {
+    print "<h4>¡Es un empate! Ambas tiradas tienen $totalTirada1 puntos.</h4>";
 }
 
 ?>
