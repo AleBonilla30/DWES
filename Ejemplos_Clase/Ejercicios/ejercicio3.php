d8b701331a6ecfb610b59f733cac6e06d69758a8
AleBonilla30 <ale.bonilla309@gmail.com>
2024-10-22 14:28:32 +0200

    Update ejercicio3.php

--- Ejemplos_Clase/Ejercicios/ejercicio3.php
+++ Ejemplos_Clase/Ejercicios/ejercicio3.php
@@ -1,9 +1,10 @@
 <?php
 //Muestra la tabla de multiplicar de un número generado de manera aleatoria entre 1 y 10. El resultado en formato <table>
 
-echo "<table border ='1' whith = 50%>";
-echo "<tr><th>Multiplicacion</th></tr>";
 $numero = rand(1,10);
+echo "<h1>La tabla de multiplicar del numero $numero</h1>";
+echo "<table border ='1' whith = 50% >";
+echo "<tr><th>Multiplicacion</th></tr>";
 for ($i=1; $i <=10 ; $i++) { 
     $mult = $numero * $i;
     echo "<tr><td>$numero x $i = $mult</td></tr>";
