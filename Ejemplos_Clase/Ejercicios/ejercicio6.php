832c1bc6f1b6047bcc45940a6bad5281256fbba0
AleBonilla30 <ale.bonilla309@gmail.com>
2024-10-22 13:14:11 +0200

    change

--- Ejemplos_Clase/Ejercicios/ejercicio6.php
+++ Ejemplos_Clase/Ejercicios/ejercicio6.php
@@ -1,5 +1,11 @@
 <?php
 /* Un programa que genere 2 tiradas de 3 dados(simulando 2 jugadores). 
 Muestre las dos tiradas y me diga cual tiene mayor puntuación(sumando las tiradas) */
+$contador = 0;
+$tirada = false;
+for ($i=0; $i <3 ; $i++) { 
+    $dado1 = rand(1,6);
+
+}
 
 ?>
