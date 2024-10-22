92b0262845b4d6d30580d8826d5abce444b0303d
AleBonilla30 <ale.bonilla309@gmail.com>
2024-10-22 14:38:37 +0200

    Update ejercicio4.php

--- Ejemplos_Clase/Ejercicios/ejercicio4.php
+++ Ejemplos_Clase/Ejercicios/ejercicio4.php
@@ -6,4 +6,15 @@ echo "<p>El numero aleatorio es: ".$numero. "</p>";
 $digitos = strlen($numero);//strlen es para calcular el numero de caracteres incluye espacios
 print " <p>La cantidad de digitos que tiene el numero es $digitos</p>";
 
+
+//se puede hacer de las dos maneras
+if ($numero <= 10){
+    print " <p>La cantidad de digitos que tiene el numero es $digitos</p>";
+}elseif ($numero <=100) {
+    print " <p>La cantidad de digitos que tiene el numero es $digitos</p>";
+}elseif ($numero <= 1000) {
+    print " <p>La cantidad de digitos que tiene el numero es $digitos</p>";
+}elseif ($numero <= 10000) {
+    print " <p>La cantidad de digitos que tiene el numero es $digitos</p>";
+}
 ?>
