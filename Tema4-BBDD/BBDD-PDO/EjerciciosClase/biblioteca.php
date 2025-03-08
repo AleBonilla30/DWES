0b044d7c11622f0c9ffec9eac56a767d5e131f13
AleBonilla30 <ale.bonilla309@gmail.com>
2025-03-08 15:52:32 +0100

    Ejercicio de clase de PDO

---
+++ Tema4-BBDD/BBDD-PDO/EjerciciosClase/biblioteca.php
@@ -0,0 +1,177 @@
+<?php
+//variables configurables por el administrador de la app
+
+require_once "config.php";
+
+//nombres de la tabla
+
+$cfg['tablaPersonas'] = 'personas'; //nombre de la tabla personas
+
+//MYSQL: funcion de conexion con la base de datos
+
+function conectaBb() {
+    global $cfg;
+    try {
+        $tmp = new PDO("mysql:host=$cfg[mysqlHost]; dbname=$cfg[mysqlDatabase]; charset=utf8mb4", $cfg["mysqlUser"], $cfg["mysqlPassword"]);
+    } catch (PDOException $e){
+        echo "<p class='aviso'>Error: No se puede conectarse con la base de datos. {$e->getMessage()}</p>\n";
+        exit;
+    } finally{
+        $tmp->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+        $tmp->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+        return $tmp;
+    }
+}
+
+//funcion borrado y creacion de la base de  datos
+
+function borrarTodo(){
+    global $pdo, $cfg;
+
+    $consulta = "DROP DATABASE IF EXISTS $cfg[mysqlDatabase]";
+
+    if (!$pdo->query($consulta)) {
+        print "<P class='aviso'>Error al borrar la base de datos. SQLSTATE[{$pdo->errorCode()}] {$pdo->errorInfo()[2]}</p>\n";
+    }else{
+        print "<p>Base de datos borrada correctamente (si existia).</p>";
+    }
+    print "\n";
+
+    $consulta = "CREATE DATABASE $cfg[mysqlDatabase] CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
+
+    if (!$pdo->query($consulta)) {
+        print "<P class='aviso'>Error al borrar la base de datos. SQLSTATE[{$pdo->errorCode()}] {$pdo->errorInfo()[2]}</p>\n";
+    }else {
+        print "<p>Base de datos creada correctamente</p>\n";
+        print "\n";
+    }
+
+    $consulta = "USE $cfg[mysqlDatabase]";
+
+    if (!$pdo->query($consulta)) {
+        print "    <p class=\"aviso\">Error en la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Base de datos seleccionada correctamente.</p>\n";
+        print "\n";
+    }
+
+    $consulta = "CREATE TABLE $cfg[tablaPersonas] (
+                id INTEGER UNSIGNED AUTO_INCREMENT,
+                nombre VARCHAR($cfg[tablaPersonasTamNombre]),
+                apellidos VARCHAR($cfg[tablaPersonasTamApellidos]),
+                PRIMARY KEY(id)
+                )";
+
+    if (!$pdo->query($consulta)) {
+        print "    <p class=\"aviso\">Error al crear la tabla. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Tabla creada correctamente.</p>\n";
+    }
+}
+
+// FUNCIÓN DE INSERCIÓN DE REGISTRO
+
+function insertaRegistro($nombre, $apellidos)
+{
+    global $pdo, $cfg;
+
+    $consulta = "INSERT INTO $cfg[tablaPersonas]
+                (nombre, apellidos)
+                VALUES (:nombre, :apellidos)";
+
+    $resultado = $pdo->prepare($consulta);
+    if (!$resultado) {
+        print "    <p class=\"aviso\">Error al preparar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } elseif (!$resultado->execute([":nombre" => $nombre, ":apellidos" => $apellidos])) {
+        print "    <p class=\"aviso\">Error al ejecutar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Registro creado correctamente.</p>\n";
+        print "\n";
+    }
+}
+
+// FUNCIÓN DE CONTEO DE REGISTROS
+
+function cuentaRegistros()
+{
+    global $pdo, $cfg;
+
+    $consulta = "SELECT COUNT(*) FROM $cfg[tablaPersonas]";
+
+    $resultado = $pdo->query($consulta);
+    if (!$resultado) {
+        print "    <p class=\"aviso\">Error en la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>La tabla contiene {$resultado->fetchColumn()} registro(s).</p>\n";
+        print "\n";
+    }
+}
+
+// FUNCIÓN DE SELECCIÓN DE TODOS LOS REGISTROS
+
+function muestraRegistros()
+{
+    global $pdo, $cfg;
+
+    $consulta = "SELECT * FROM $cfg[tablaPersonas]";
+
+    $resultado = $pdo->query($consulta);
+    if (!$resultado) {
+        print "    <p class=\"aviso\">Error en la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p><strong>Registro(s) obtenido(s):</strong></p>\n";
+        print "    <ul>\n";
+        foreach ($resultado as $registro) {
+            print "      <li>$registro[id] - $registro[nombre] - $registro[apellidos]</li>\n";
+        }
+        print "    </ul>\n";
+        print "\n";
+    }
+}
+
+// FUNCIÓN DE MODIFICACIÓN DE REGISTRO
+
+function modificaRegistro($id, $nombre, $apellidos)
+{
+    global $pdo, $cfg;
+
+    $consulta = "UPDATE $cfg[tablaPersonas]
+                SET nombre = :nombre, apellidos = :apellidos
+                WHERE id = :id";
+
+    $resultado = $pdo->prepare($consulta);
+    if (!$resultado) {
+        print "    <p class=\"aviso\">Error al preparar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } elseif (!$resultado->execute([":nombre" => $nombre, ":apellidos" => $apellidos, ":id" => $id])) {
+        print "    <p class=\"aviso\">Error al ejecutar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Registro modificado correctamente.</p>\n";
+        print "\n";
+    }
+}
+
+// FUNCIÓN DE BORRADO DE REGISTROS
+
+function borraRegistros($id)
+{
+    global $pdo, $cfg;
+
+    foreach ($id as $indice => $valor) {
+        $consulta = "DELETE FROM $cfg[tablaPersonas]
+                    WHERE id = :indice";
+
+        $resultado = $pdo->prepare($consulta);
+        if (!$resultado) {
+            print "    <p class=\"aviso\">Error al preparar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+        } elseif (!$resultado->execute([":indice" => $indice])) {
+            print "    <p class=\"aviso\">Error al ejecutar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+        } else {
+            print "    <p>Registro borrado correctamente (si existía).</p>\n";
+            print "\n";
+        }
+    }
+}
+
+
+
+?>
---
+++ Tema4-BBDD/BBDD-PDO/EjerciciosClase/config.php
@@ -0,0 +1,12 @@
+<?php
+//variables configurables por el administrador de la aplicacion
+
+//configuracion para Mysql
+
+$cfg["mysqlHost"] = "localhost"; //nombre del host
+$cfg["mysqlUser"] = "root"; //nombre de usuario
+$cfg["mysqlPassword"] = "";//contraseña
+$cfg["mysqlDatabase"] = "clase43";//nombre de la base de datos
+
+
+?>
